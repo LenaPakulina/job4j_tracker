a6c96192e3aa3dfc89dcf3d9af338507a7a9e640
Elena Pakulina <el.pakulina@yandex.ru>
2023-12-09 23:32:02 +0500

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,22 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int sizeByName = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null
+                    && items[i].getName().equals(key)) {
+                rsl[sizeByName] = items[i];
+                sizeByName++;
+            }
+        }
+        return Arrays.copyOf(rsl, sizeByName);
     }
 }
