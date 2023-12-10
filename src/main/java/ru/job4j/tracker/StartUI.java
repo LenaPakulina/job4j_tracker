28d88246b8f8375992b3bbfa2c0e71dc8df72abb
Elena Pakulina <el.pakulina@yandex.ru>
2023-12-10 21:22:03 +0500

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,10 +6,13 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
+        System.out.println("8. Date. Отображение даты:");
         LocalDateTime dateTime = item.getCreated();
         DateTimeFormatter formatter =
                 DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = dateTime.format(formatter);
         System.out.println(currentDateTimeFormat);
+        System.out.println("10. toString [#310018]:");
+        System.out.println(item);
     }
 }
