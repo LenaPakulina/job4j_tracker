146125a1ff0274888730da223dbe900e5476e7e8
Elena Pakulina <el.pakulina@yandex.ru>
2023-12-17 21:20:51 +0500

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key.equals(value[i])) {
+                return i;
+            }
+        }
+        if (result < 0) {
+            throw new ElementNotFoundException("Index was not found.");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] array = new String[] {"a", "b", "c"};
+            FindEl.indexOf(array, "b");
+            FindEl.indexOf(array, "v");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
