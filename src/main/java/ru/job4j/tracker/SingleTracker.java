fc1eb0d4be841776dc084716472fb9dc545ac154
Elena Pakulina <el.pakulina@yandex.ru>
2023-12-23 19:52:54 +0500

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,50 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public final class SingleTracker {
+    private static Tracker tracker = new Tracker();
+    private static SingleTracker singleTracker = null;
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getSingleTracker() {
+        if (singleTracker == null) {
+            singleTracker = new SingleTracker();
+        }
+        return singleTracker;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
+    }
+
+    public static void main(String[] args) {
+        SingleTracker sglTracker = SingleTracker.getSingleTracker();
+        sglTracker.add(new Item("sdfsdf"));
+        sglTracker.add(new Item("sdfsdf"));
+        sglTracker.add(new Item("sdfsdf"));
+        System.out.println("Size = " + sglTracker.findAll().length);
+    }
+}
