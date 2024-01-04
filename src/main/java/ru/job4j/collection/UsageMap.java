6c90e5b6dc2df1a30af037bc09c16aab79c75985
Elena Pakulina <el.pakulina@yandex.ru>
2024-01-04 13:49:59 +0500

    0. Map, HashMap [#212652 #470397]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("el.pakulina@yandex.ru", "Elena Pakulina");
+        map.put("el.pakulina@yandex.ru", "Rita Pakulina");
+        map.put("El.pakulina@yandex.ru", "ELENA Pakulina");
+        map.put("moli@america.com", "John Winston Lennon");
+        map.put("sara@ldsd.com", "Sarah Hall");
         for (Map.Entry<String, String> entry : map.entrySet()) {
             System.out.println(entry.getKey() + " = " + entry.getValue());
         }
