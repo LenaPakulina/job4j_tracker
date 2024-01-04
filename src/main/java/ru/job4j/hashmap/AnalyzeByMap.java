231c66573b3a61eb5545c7dcc48edd49b3a68f57
Elena Pakulina <el.pakulina@yandex.ru>
2024-01-04 17:14:42 +0500

    4. Аттестация [#504878]

---
+++ src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
@@ -0,0 +1,77 @@
+package ru.job4j.hashmap;
+
+import java.util.*;
+
+public class AnalyzeByMap {
+    private static Map<String, Integer> getSubjectsInfo(List<Pupil> pupils) {
+        Map<String, Integer> scopes = new HashMap<>();
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                if (scopes.containsKey(subject.name())) {
+                    Integer value = scopes.get(subject.name()) + subject.score();
+                    scopes.put(subject.name(), value);
+                } else {
+                    scopes.put(subject.name(), subject.score());
+                }
+            }
+        }
+        return scopes;
+    }
+
+    public static double averageScore(List<Pupil> pupils) {
+        int count = 0;
+        double sum = 0D;
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                count++;
+                sum += subject.score();
+            }
+        }
+        return count == 0 ? sum : (sum / count);
+    }
+
+    public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
+        List<Label> labels = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            double sum = 0D;
+            for (Subject subject : pupil.subjects()) {
+                sum += subject.score();
+            }
+            int count = pupil.subjects().size();
+            labels.add(new Label(pupil.name(), (count == 0 ? sum : (sum / count))));
+        }
+        return labels;
+    }
+
+    public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
+        Map<String, Integer> scopes = getSubjectsInfo(pupils);
+        List<Label> labels = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : scopes.entrySet()) {
+            labels.add(new Label(entry.getKey(), (double) entry.getValue() / pupils.size()));
+        }
+        return labels;
+    }
+
+    public static Label bestStudent(List<Pupil> pupils) {
+        List<Label> labels = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            double sumPupilScore = 0D;
+            for (Subject subject : pupil.subjects()) {
+                sumPupilScore += subject.score();
+            }
+            labels.add(new Label(pupil.name(), sumPupilScore));
+        }
+        labels.sort(Comparator.naturalOrder());
+        return labels.isEmpty() ? null : labels.get(labels.size() - 1);
+    }
+
+    public static Label bestSubject(List<Pupil> pupils) {
+        Map<String, Integer> scopes = getSubjectsInfo(pupils);
+        List<Label> labels = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : scopes.entrySet()) {
+            labels.add(new Label(entry.getKey(), entry.getValue()));
+        }
+        labels.sort(Comparator.naturalOrder());
+        return labels.isEmpty() ? null : labels.get(labels.size() - 1);
+    }
+}
