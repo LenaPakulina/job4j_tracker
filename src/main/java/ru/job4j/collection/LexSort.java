89dcb8966d102346fd0c02fd25e87ee25734db2f
Elena Pakulina <el.pakulina@yandex.ru>
2024-01-07 19:48:57 +0500

    4. Сортировка номера [#221284 #471124]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -3,16 +3,10 @@ package ru.job4j.collection;
 import java.util.Comparator;
 
 public class LexSort implements Comparator<String> {
-
     @Override
     public int compare(String left, String right) {
-        String[] aLeft = left.split("\\.");
-        String[] aRight = right.split("\\.");
-        if (aLeft.length == 0 || aRight.length == 0) {
-            return aLeft.length == 0 ? -1 : 1;
-        }
-        Integer i1 = Integer.parseInt(aLeft[0]);
-        Integer i2 = Integer.parseInt(aRight[0]);
+        Integer i1 = Integer.parseInt(left.split("\\.")[0]);
+        Integer i2 = Integer.parseInt(right.split("\\.")[0]);
         return i1.compareTo(i2);
     }
 }
